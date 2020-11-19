ecef25014e30c9c446fece74f56c5fae70e9261f
Ruben Wisskott <ruben.wisskott@gmx.de>
2020-11-19 13:53:56 +0100

    Try it!!

---
+++ complex_KI_code.R
@@ -0,0 +1,17 @@
+# secret code: Inspection only with permission!
+
+answer <- function(string) {
+  if(string == "Whats the answer of everything?"){
+    return(42)
+  }
+  else{
+    return("Ask the right question!")
+  }
+}
+
+
+
+# Ask deep thought the following question:
+# "Whats the answer of everything?"
+
+answer("Whats the answer of everything?")
